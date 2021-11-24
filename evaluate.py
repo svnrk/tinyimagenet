4c214c568ea254fd9e79880683713600a3544b4e
svnrk <svenerikmandmaa@gmail.com>
2021-11-24 20:30:02 +0200

    new evaluate

--- evaluate.py
+++ evaluate.py
@@ -3,13 +3,16 @@ import logging
 from pathlib import Path
 from typing import Tuple, Union
 
+import albumentations as albu
 import numpy as np
 import torch
+from hydra.utils import instantiate, to_absolute_path
 from omegaconf import OmegaConf
 from torch.nn.modules import loss
+from torch.utils.data import DataLoader
 
 from modules.dataset import DatasetItem, TinyImagenetDataset
-from modules.runner import test, torch_model
+from modules.runner import test
 from modules.transform import to_tensor_normalize
 
 
@@ -36,7 +39,7 @@ def evaluate_model(
     log.info(f"Read config from {cfg_path}")
 
     cfg = OmegaConf.load(str(cfg_path))
-    log.debug(f"Config:\n{cfg.pretty()}")
+    log.info(f"Config:\n{OmegaConf.to_yaml(cfg)}")
 
     # Specify results paths from config
     checkpoint_path = results_root / cfg.results.checkpoints.root
@@ -56,14 +59,25 @@ def evaluate_model(
         )
         raise FileNotFoundError
 
-    base_transform = to_tensor_normalize()
-    test_dataset = TinyImagenetDataset(test_path, cfg, base_transform)
-    test_loader = torch.utils.data.DataLoader(
+    valid_transform = to_tensor_normalize()
+    if "augmentation" in cfg:
+        pre_transform = albu.load(
+            to_absolute_path(cfg.augmentation.pre), data_format="yaml"
+        )
+        post_transform = albu.load(
+            to_absolute_path(cfg.augmentation.post), data_format="yaml"
+        )
+        valid_transform = albu.Compose([pre_transform, post_transform])
+
+    test_dataset = TinyImagenetDataset(test_path, cfg.data, valid_transform)
+    test_loader = DataLoader(
         test_dataset,
         batch_size=cfg.train.batch_size,
         shuffle=False,
         collate_fn=DatasetItem.collate,
         num_workers=cfg.train.num_workers,
+        pin_memory=True,
+        drop_last=False,
     )
 
     log.info(
@@ -74,13 +88,7 @@ def evaluate_model(
     )
 
     loss_function = loss.CrossEntropyLoss()
-    model = torch_model(
-        cfg.model.arch,
-        cfg.data.classes,
-        cfg.model.pretrained,
-        log,
-        module_name=cfg.model.module,
-    )
+    model = instantiate(cfg.model)
     try:
         model.load_state_dict(torch.load(checkpoint_path, map_location="cpu"))
     except RuntimeError as e:
@@ -90,10 +98,8 @@ def evaluate_model(
         log.error("Checkpoint not found")
         raise e
     log.info(f"Loaded model from {checkpoint_path}")
-    device = (
-        torch.device("cuda")
-        if device == "cuda" and torch.cuda.is_available()
-        else torch.device("cpu")
+    device = torch.device(
+        "cuda" if device == "cuda" and torch.cuda.is_available() else "cpu"
     )
     test_loss, test_acc, test_outputs = test(
         model, device, test_loader, loss_function, 0, log
